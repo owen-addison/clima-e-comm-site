21f19fc75c73051a2a1d0368b788b7c22611a86a
Owen Lewis <owenalewis@gmail.com>
2024-03-26 17:32:14 +0100

    [feat] Create a component for the page header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import './App.css';
 
+function Header() {
+  return (
+    <>
+      <div className="header min-w-screen flex h-28 content-center justify-between p-4">
+        <div id="logo-container">Clima</div>
+        <div id="header-links-container">
+          <div className="about-link">About</div>
+        </div>
+      </div>
+    </>
+  );
+}
+
 function App() {
   return (
     <>
-      <h1 className="p-8 text-3xl font-bold text-emerald-800">Clima Webshop</h1>
+      <Header />
     </>
   );
 }
