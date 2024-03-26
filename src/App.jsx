752a0247e6bf325dfe0b26bcf81abae08d98a778
Owen Lewis <owenalewis@gmail.com>
2024-03-26 16:18:56 +0100

    [style] Change text colour

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold">Clima Webshop</h1>
+      <h1 className="text-3xl font-bold text-emerald-800">Clima Webshop</h1>
     </>
   );
 }
