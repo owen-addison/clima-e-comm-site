b12b99900b7b17f86c2f789fd99b200ad96c8775
Owen Lewis <owenalewis@gmail.com>
2024-03-26 17:49:48 +0100

    [refactor] Move header div and children to index - Move header div and its children into the index.html file. - Delete the Header component from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,20 @@
 import './App.css';
 
-function Header() {
-  return (
-    <>
-      <div className="header min-w-screen flex h-28 content-center justify-between p-4">
-        <div id="logo-container">Clima</div>
-        <div id="header-links-container">
-          <div className="about-link">About</div>
-        </div>
-      </div>
-    </>
-  );
-}
+// function Header() {
+//   return (
+//     <>
+//       <div className="header min-w-screen flex h-auto content-center justify-between">
+//         <div id="logo-container">Clima</div>
+//         <div id="header-links-container">
+//           <div className="about-link">About</div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
 
 function App() {
-  return (
-    <>
-      <Header />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
