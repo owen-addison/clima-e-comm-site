9c9bce9655fefe9ffe1905e3ab4da1ccb99f7c24
Owen Lewis <owenalewis@gmail.com>
2024-03-27 15:43:19 +0100

    [style] Change style of header container div Change the style of the header container div so that it removes padding at the top and adds a bottom border line.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 function Header() {
   return (
     <>
-      <div className="header min-w-screen flex h-auto content-center justify-between">
+      <div className="header min-w-screen flex h-auto content-center justify-between border-b border-lime-800 pt-0">
         <div id="logo-container">Clima</div>
         <div id="header-links-container">
           <div className="about-link">About</div>
