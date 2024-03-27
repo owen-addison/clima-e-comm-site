5ffa7b916ab7ad6d8f5a3384055fefc28646a2d3
Owen Lewis <owenalewis@gmail.com>
2024-03-27 15:24:30 +0100

    [feat] Loads header as a component - Removes header as static element in index.html. - Adds as a "Header" component in App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
 import './App.css';
 
-// function Header() {
-//   return (
-//     <>
-//       <div className="header min-w-screen flex h-auto content-center justify-between">
-//         <div id="logo-container">Clima</div>
-//         <div id="header-links-container">
-//           <div className="about-link">About</div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
+function Header() {
+  return (
+    <>
+      <div className="header min-w-screen flex h-auto content-center justify-between">
+        <div id="logo-container">Clima</div>
+        <div id="header-links-container">
+          <div className="about-link">About</div>
+        </div>
+      </div>
+    </>
+  );
+}
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
