6bf25617a6412aefce6d350e79440cc819a443eb
Owen Lewis <owenalewis@gmail.com>
2024-03-29 21:44:11 +0100

    [style] Update style of header elements

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,22 @@ import './App.css';
 function Header() {
   return (
     <>
-      <div className="header min-w-screen flex h-auto content-center justify-between border-b border-lime-800 pt-0">
-        <div id="logo-container" className="font-sans">
+      <div className="header min-w-screen border-moss-800 flex h-auto content-center justify-between border-b pt-0">
+        <div
+          id="logo-container"
+          className="text-moss-950 font-sans text-2xl font-light tracking-widest"
+        >
           Clima
         </div>
-        <div id="header-links-container">
-          <div className="about-link">About</div>
+        <div
+          id="header-links-container"
+          className="flex flex-row items-center justify-center"
+        >
+          <div className="flex items-center justify-center">
+            <p className="text-moss-950 font-sans text-base font-light tracking-wide">
+              About
+            </p>
+          </div>
         </div>
       </div>
     </>
