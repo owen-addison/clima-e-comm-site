0b1ca957b8ad4587192a40029e5927cabda1ec11
Owen Lewis <owenalewis@gmail.com>
2024-04-01 12:35:56 +0200

    Delete commented out code

--- src/Item.jsx
+++ src/Item.jsx
@@ -55,26 +55,3 @@ function Item() {
 }
 
 export default Item;
-
-/*
-<div className="item-container flex content-center justify-center gap-12 p-8">
-        <div className="item-lhs">
-          <img
-            src="./src/assets/soap_front_crop.png"
-            alt="Item image"
-            className="item-image h-auto w-80"
-          />
-        </div>
-        <div className="item-rhs flex w-80 flex-col content-center justify-center gap-4">
-          <div className="item-details-container">
-            <h3 className="item-header font-semibold">Soap</h3>
-            <ul className="item-ingredients">
-              <li className="ingredient">Ingredient 1</li>
-              <li className="ingredient">Ingredient 2</li>
-              <li className="ingredient">Ingredient 3</li>
-            </ul>
-          </div>
-          <button className="add-button">Add to basket</button>
-        </div>
-      </div>
-*/
