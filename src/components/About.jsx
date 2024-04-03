bfd8869e7abdb630a2034780cc7a0e51086c7b77
Owen Lewis <owenalewis@gmail.com>
2024-04-03 10:36:44 +0200

    [style] Restyle the About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,13 +3,15 @@ import PropTypes from 'prop-types';
 function About({ onClose }) {
   return (
     <>
-      <div className="fixed inset-0 z-10 flex flex-col items-start justify-between bg-moss-700">
-        <button
-          onClick={onClose}
-          className="m-4 rounded bg-red-600 p-2 text-white hover:bg-red-700"
-        >
-          Close
-        </button>
+      <div className="about fixed inset-0 z-10 flex flex-col items-start justify-between bg-moss-400">
+        <div className="about-header flex w-full flex-row justify-end px-4">
+          <button
+            onClick={onClose}
+            className="mx-4 rounded bg-moss-700 px-4 py-2 text-white hover:bg-moss-900"
+          >
+            Close
+          </button>
+        </div>
         {/* Your about content here */}
       </div>
     </>
