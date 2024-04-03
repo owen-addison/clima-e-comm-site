0c3786aaffcc3d3211e8eccee86cdf4961c0c74a
Owen Lewis <owenalewis@gmail.com>
2024-04-03 09:23:27 +0200

    [feat] Add a close button and styling to About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,24 @@
-function About() {
+import PropTypes from 'prop-types';
+
+function About({ onClose }) {
   return (
     <>
-      <div className="min-w-screen min-h-screen bg-moss-900"></div>
+      <div className="fixed inset-0 z-10 flex flex-col items-start justify-between bg-moss-700">
+        <button
+          onClick={onClose}
+          className="m-4 rounded bg-red-600 p-2 text-white hover:bg-red-700"
+        >
+          Close
+        </button>
+        {/* Your about content here */}
+      </div>
     </>
   );
 }
+
+// Define the expected prop types
+About.propTypes = {
+  onClose: PropTypes.func.isRequired,
+};
 
 export default About;
