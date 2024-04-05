0a7e0f6d81f2a025ab104f82d6291b69e137b33d
Owen Lewis <owenalewis@gmail.com>
2024-04-05 15:21:04 +0200

    [content] Change content structure, add Contact - Change content structure to house both About and Contact sections. - Add the Contact section.

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -13,35 +13,40 @@ function Info({ onClose }) {
           </button>
         </div>
         <div className="info-body flex flex-col items-center justify-center p-8">
-          <h1 className="text-xl font-medium">ABOUT</h1>
-          <div className="about-text flex w-4/6 flex-col gap-6 p-8">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Et
-              malesuada fames ac turpis egestas sed. Sit amet aliquam id diam.
-              Elementum integer enim neque volutpat. Ipsum nunc aliquet bibendum
-              enim facilisis gravida neque. Faucibus et molestie ac feugiat sed
-              lectus vestibulum mattis. Id interdum velit laoreet id. Egestas
-              quis ipsum suspendisse ultrices gravida dictum. Arcu non sodales
-              neque sodales ut etiam sit amet. Tortor at auctor urna nunc.
-              Egestas diam in arcu cursus euismod quis. Non odio euismod lacinia
-              at quis risus sed vulputate odio. Et malesuada fames ac turpis
-              egestas integer eget aliquet nibh.
-            </p>
-            <p>
-              Id diam maecenas ultricies mi eget mauris. Pellentesque eu
-              tincidunt tortor aliquam nulla facilisi cras. Nibh tortor id
-              aliquet lectus proin nibh. Pharetra diam sit amet nisl suscipit
-              adipiscing bibendum est ultricies. Nibh nisl condimentum id
-              venenatis a condimentum. Proin fermentum leo vel orci porta non
-              pulvinar. Auctor elit sed vulputate mi sit. Eu ultrices vitae
-              auctor eu augue ut. Turpis egestas maecenas pharetra convallis
-              posuere morbi leo urna molestie. Commodo viverra maecenas accumsan
-              lacus vel facilisis. Venenatis lectus magna fringilla urna.
-              Aliquam sem fringilla ut morbi tincidunt. Enim blandit volutpat
-              maecenas volutpat blandit aliquam. Elit duis tristique
-              sollicitudin nibh sit.
-            </p>
+          <div className="about-section flex flex-col items-center justify-center p-8">
+            <h1 className="text-xl font-medium">ABOUT</h1>
+            <div className="about-text flex w-4/6 flex-col gap-6 p-8">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Et
+                malesuada fames ac turpis egestas sed. Sit amet aliquam id diam.
+                Elementum integer enim neque volutpat. Ipsum nunc aliquet
+                bibendum enim facilisis gravida neque. Faucibus et molestie ac
+                feugiat sed lectus vestibulum mattis. Id interdum velit laoreet
+                id. Egestas quis ipsum suspendisse ultrices gravida dictum. Arcu
+                non sodales neque sodales ut etiam sit amet. Tortor at auctor
+                urna nunc. Egestas diam in arcu cursus euismod quis. Non odio
+                euismod lacinia at quis risus sed vulputate odio. Et malesuada
+                fames ac turpis egestas integer eget aliquet nibh.
+              </p>
+              <p>
+                Id diam maecenas ultricies mi eget mauris. Pellentesque eu
+                tincidunt tortor aliquam nulla facilisi cras. Nibh tortor id
+                aliquet lectus proin nibh. Pharetra diam sit amet nisl suscipit
+                adipiscing bibendum est ultricies. Nibh nisl condimentum id
+                venenatis a condimentum. Proin fermentum leo vel orci porta non
+                pulvinar. Auctor elit sed vulputate mi sit. Eu ultrices vitae
+                auctor eu augue ut. Turpis egestas maecenas pharetra convallis
+                posuere morbi leo urna molestie. Commodo viverra maecenas
+                accumsan lacus vel facilisis. Venenatis lectus magna fringilla
+                urna. Aliquam sem fringilla ut morbi tincidunt. Enim blandit
+                volutpat maecenas volutpat blandit aliquam. Elit duis tristique
+                sollicitudin nibh sit.
+              </p>
+            </div>
+            <div className="contact-section flex flex-col items-center justify-center p-8">
+              <h1 className="text-xl font-medium">CONTACT</h1>
+            </div>
           </div>
         </div>
       </div>
